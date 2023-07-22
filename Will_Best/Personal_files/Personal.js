0784aa7bcdacb30c73b0c8d90e26516003509192
Will Best <wbest@utexas.edu>
2023-07-22 17:14:39 -0500

    first commit of old IfA webpage

---
+++ Will_Best/Personal_files/Personal.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170102
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Personal_files/PersonalMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
