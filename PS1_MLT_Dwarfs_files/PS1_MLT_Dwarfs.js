a7cec91f6667b56954a77562055f90cce78e7e87
Will Best <wbest@utexas.edu>
2023-07-30 20:16:39 -0500

    New UT webpage files.

---
+++ PS1_MLT_Dwarfs_files/PS1_MLT_Dwarfs.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20181004
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('PS1_MLT_Dwarfs_files/PS1_MLT_DwarfsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
